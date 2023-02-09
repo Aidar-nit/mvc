99960673efaedd1106f9e3fbad20094fb7be6e66
Aidar-nit <mp.uu.ak@gmail.com>
2023-02-09 09:38:11 +0600

    modified: file routes.php add new url

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
-    '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main'],
+    '~^articles/(\d+)$~' => [\MyProject\Controllers\ArticlesController::class, 'view'],
+
 ];
